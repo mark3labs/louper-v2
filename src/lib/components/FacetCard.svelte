f1563850b24b294423a1c6d5173d9904fe41510d
Ed Zynda <ezynda3@gmail.com>
2022-02-04 11:51:16 +0300

    🔧 allow removal of unverified facets

--- src/lib/components/FacetCard.svelte
+++ src/lib/components/FacetCard.svelte
@@ -77,8 +77,8 @@
         {/each}
       </table>
     </div>
-    {#if facet.name}
-      <div class="card-actions bg-secondary rounded-md p-1 text-secondary-content">
+    <div class="card-actions bg-secondary rounded-md p-1 text-secondary-content">
+      {#if facet.name}
         <button
           class="btn glass btn-xs"
           on:click={() => {
@@ -125,32 +125,32 @@
           </svg>
           Write
         </button>
-        <button
-          class="btn glass btn-xs bg-error"
-          on:click={() => {
-            showReadContract = false
-            showWriteContract = false
-            showRemoveFacet = true
-            activeFacet = facet
-          }}
+      {/if}
+      <button
+        class="btn glass btn-xs bg-error"
+        on:click={() => {
+          showReadContract = false
+          showWriteContract = false
+          showRemoveFacet = true
+          activeFacet = facet
+        }}
+      >
+        <svg
+          class="w-4 h-4 mr-1"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <svg
-            class="w-4 h-4 mr-1"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-            />
-          </svg>
-          Remove
-        </button>
-      </div>
-    {/if}
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+          />
+        </svg>
+        Remove
+      </button>
+    </div>
   </div>
 </div>
