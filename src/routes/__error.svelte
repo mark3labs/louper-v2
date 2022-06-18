a177f84cd1519b6b24c7c2080ff415ef3615021f
Ed Zynda <ezynda3@gmail.com>
2022-06-18 18:13:09 +0300

    🔧 update error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,1 +1,21 @@
-Uh oh...
+<div class="flex items-center justify-center">
+  <div class="px-40 py-20 bg-base-300 rounded-md shadow-xl">
+    <div class="flex flex-col items-center">
+      <h1 class="font-bold text-base-content text-4xl">💀</h1>
+
+      <h6 class="mb-2 text-2xl font-bold text-center text-base-content md:text-3xl">
+        <span class="text-error">Oops!</span> Could not load diamond
+      </h6>
+
+      <p class="mb-2 text-center text-gray-500 md:text-lg">
+        Are you sure this is a diamond contract?
+      </p>
+      <p class="mb-8 text-center text-gray-500 md:text-lg">
+        Please make sure that the contract has a "DiamondLoupeFacet" that implements the "facets()"
+        method.
+      </p>
+
+      <a href="/" class="btn btn-primary font-semibold">Go home</a>
+    </div>
+  </div>
+</div>
