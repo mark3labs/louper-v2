e34b27b96688c3881e3c4e9e903d4db366c2be59
Ed Zynda <ezynda3@gmail.com>
2022-06-28 16:46:17 +0300

    add more fidelity to history

--- src/lib/components/History.svelte
+++ src/lib/components/History.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import type { LouperEvent } from '../../types/entities'
-  import { getExplorerTxUrl } from '$lib/utils'
+  import { getExplorerTxUrl, getExplorerAddressUrl } from '$lib/utils'
   export let events: LouperEvent[] = []
   export let network
+
+  const ACTIONS = ['Add', 'Replace', 'Remove']
 </script>
 
 <div class="rounded-box bg-base-300 text-base-content collapse collapse-arrow">
@@ -58,6 +60,43 @@
                 />
               </svg>
             </a>
+            {#each event[0] as cut}
+              <div class="flex my-2 space-x-1 border-t border-gray-600 pt-3">
+                <div
+                  class="badge badge-lg text-white uppercase font-semibold"
+                  class:badge-primary={cut[1] == 0}
+                  class:badge-secondary={cut[1] == 1}
+                  class:badge-accent={cut[1] == 2}
+                >
+                  {ACTIONS[cut[1]]}
+                </div>
+                <a
+                  href={getExplorerAddressUrl(event.txHash, network)}
+                  class="badge badge-info p-3 cursor-pointer text-xs lg:text-base"
+                  target="_blank"
+                >
+                  {cut[0]}
+                  <svg
+                    class="w-3 h-3 md:w-4 md:h-4 inline ml-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+                    />
+                    <path
+                      d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+                    />
+                  </svg>
+                </a>
+              </div>
+              <div class="flex flex-wrap space-x-1 ml-16">
+                {#each cut[2] as selector}
+                  <span class="badge badge-info badge-outline font-bold my-2">{selector}</span>
+                {/each}
+              </div>
+            {/each}
           </div>
         </li>
       {/each}
