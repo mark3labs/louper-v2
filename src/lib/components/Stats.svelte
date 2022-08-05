9da54d182ede89702566cd3603dd054e5c2ef044
Ed Zynda <ezynda3@gmail.com>
2022-08-05 17:32:00 +0300

    Add some simple stats

---
+++ src/lib/components/Stats.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { onMount } from 'svelte'
+
+  let diamondCount = 0
+  let contractCount = 0
+
+  onMount(async () => {
+    const res = await fetch('/api/stats')
+    ;({ diamondCount, contractCount } = await res.json())
+  })
+</script>
+
+<div class="stats">
+  <div class="stat">
+    <div class="stat-figure text-3xl">💎</div>
+    <div class="stat-title">Diamonds Inspected</div>
+    <div class="stat-value">{diamondCount}</div>
+  </div>
+
+  <div class="stat">
+    <div class="stat-figure text-3xl">🔷</div>
+    <div class="stat-title">Contracts Inspected</div>
+    <div class="stat-value">{contractCount}</div>
+  </div>
+
+  <!-- <div class="stat"> -->
+  <!--   <div class="stat-figure text-3xl"> -->
+  <!--   👀 -->
+  <!--   </div> -->
+  <!--   <div class="stat-title">Total Views</div> -->
+  <!--   <div class="stat-value">1,200</div> -->
+  <!-- </div> -->
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,13 @@
 <script>
   import Header from '$lib/components/Header.svelte'
   import Featured from '$lib/components/Featured.svelte'
+import Stats from '$lib/components/Stats.svelte'
 </script>
 
 <div class="flex flex-col w-full space-y-10 my-5">
   <Header />
-  <!-- Featured Diamonds -->
+  <!-- Stats -->
+  <Stats />
+<!-- Featured Diamonds -->
   <Featured />
 </div>
