1dfd707c8485c594d05cd8691a035c068038d102
Ed Zynda <ezynda3@gmail.com>
2022-08-09 16:11:47 +0300

    add polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 /** @type {import('vite').UserConfig} */
 const config = {
+  resolve: {
+    alias: {
+      events: 'rollup-plugin-node-polyfills/polyfills/events',
+      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+    },
+  },
   plugins: [sveltekit()],
   server: {
     port: 3000,
@@ -9,16 +19,24 @@ const config = {
   optimizeDeps: {
     esbuildOptions: {
       target: 'es2020',
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
     },
   },
   build: {
     target: 'es2020',
     rollupOptions: {
-      external: ['named-logs'],
+      external: ['web3w'],
+      plugins: [rollupNodePolyFill()],
     },
-  },
-  define: {
-    global: {},
   },
 }
 
