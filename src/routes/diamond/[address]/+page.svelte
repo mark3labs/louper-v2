67bdf61960a1e00e546ae94c427ee61e379b83f8
Ed Zynda <ezynda3@gmail.com>
2023-05-02 19:13:41 +0300

    fix: change button text to reflect actual functionality

--- src/routes/diamond/[address]/+page.svelte
+++ src/routes/diamond/[address]/+page.svelte
@@ -145,7 +145,7 @@
               d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"
             />
           </svg>
-          Upgrade Facet
+          Add New Facet
         </button>
         {#if $user}
           <Bookmark {diamond} />
